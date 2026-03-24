67c386a98c28dd32379ffcd1e6947ec011c4924e
chiheb-xhbb <chihebddine.selmi@etudiant-fst.utm.tn>
2026-03-24 22:15:54 +0100

    feat(api): add dossier routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DocumentController;
 use App\Http\Controllers\Api\DocumentValidationController;
+use App\Http\Controllers\Api\DossierController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -32,3 +34,11 @@ Route::middleware('auth:sanctum')->prefix('documents')->group(function () {
     Route::post('/{document}/retry', [DocumentController::class, 'retry']);
 });
 
+Route::middleware('auth:sanctum')->prefix('dossiers')->group(function () {
+    Route::get('/', [DossierController::class, 'index']);
+    Route::post('/', [DossierController::class, 'store']);
+    Route::get('/{dossier}', [DossierController::class, 'show']);
+    Route::put('/{dossier}', [DossierController::class, 'update']);
+    Route::delete('/{dossier}', [DossierController::class, 'destroy']);
+    Route::post('/{dossier}/attach-documents', [DossierController::class, 'attachDocuments']);
+});
